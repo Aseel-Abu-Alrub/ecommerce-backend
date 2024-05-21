ad1e2c4476d806b928017d69a1fe841d274b7ca1
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2024-05-22 01:39:24 +0300

    Ecommerce

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -2,13 +2,13 @@ import cartModel from "../../../DB/model/cart.model.js"
 import productModel from "../../../DB/model/product.model.js"
 
 export const createCart=async(req,res,next)=>{
-     const productId=req.params.id
-    const{quantity}=req.body
+    //  const productId=req.params.id
+    const{quantity,productId}=req.body
     
-    if(!await productModel.findById({_id:productId})){
-        return res.status(200).json({message:"product not found"})
+    // if(!await productModel.findById({_id:productId})){
+    //     return res.status(200).json({message:"product not found"})
 
-    }
+    // }
     const cart=await cartModel.findOne({userId:req.user._id})
 
     if(!cart){
