159b02c74e854baefc82d201e8942d626697a353
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2024-05-23 01:49:41 +0300

    Ecommerce

--- src/app.router.js
+++ src/app.router.js
@@ -14,7 +14,7 @@ const initApp=(app,express)=>{
     app.use(express.json())
     connectDB()
     app.use(cors({
-        origin:'*',
+        origin:'http://localhost:3000',
         credentials: true,
          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         // allowedHeaders: ['Content-Type', 'Authorization','origin','X-Requested-With','Accept'],
