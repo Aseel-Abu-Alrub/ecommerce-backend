a8570efdd65598d652b98e167061a8c857ee678a
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2024-05-23 01:27:51 +0300

    Ecommerce

--- src/app.router.js
+++ src/app.router.js
@@ -18,6 +18,7 @@ const initApp=(app,express)=>{
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Request-Method'],
+        preflightContinue: true,
         optionsSuccessStatus: 204
     }))
 
