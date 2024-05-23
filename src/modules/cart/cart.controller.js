5f00fded44990a11c3629b5bac74eff9b5fa19e3
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2024-05-23 13:05:37 +0300

    Ecommerce

--- src/modules/cart/cart.controller.js
+++ src/modules/cart/cart.controller.js
@@ -2,7 +2,7 @@ import cartModel from "../../../DB/model/cart.model.js"
 
 export const createCart=async(req,res,next)=>{
     const{quantity,productId}=req.body
-
+   return res.json({productId})
     
     const cart=await cartModel.findOne({userId:req.user._id})
 
