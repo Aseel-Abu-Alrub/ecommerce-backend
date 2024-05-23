54acb8a4f0bbd8cfbc606737bac6461d734b17a6
MohammedJarrad <mohammed.w.jarrad@gmail.com>
2024-05-23 22:51:23 +0300

    fix api errors

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,41 +1,40 @@
-import jwt from 'jsonwebtoken'
-import userModel from '../../DB/model/user.model.js'
+import jwt from "jsonwebtoken";
+import userModel from "../../DB/model/user.model.js";
 
-export const roles={
-    Admin:'Admin',
-    User:'User',
-    HR:'HR'
-}
+export const roles = {
+    Admin: "Admin",
+    User: "User",
+    HR: "HR",
+};
 
-export const auth=(accessRole=[])=>{
-return async(req,res,next)=>{
-    const{authorization}=req.headers
-    if(!authorization?.startsWith(process.env.BEARERKEY)){
-        return res.status(400).json({message:"Invalid authorization"})
-    }
-    const token=authorization.split(process.env.BEARERKEY)[1]
-    const decoded=jwt.verify(token,process.env.LOGINTOKEN)
-    if(!decoded){
-        return res.status(400).json({message:"Invalid authorization"})
+export const auth = (accessRole = []) => {
+    return async (req, res, next) => {
+        const { authorization } = req.headers;
+        if (!authorization?.startsWith(process.env.BEARERKEY)) {
+            return res.status(400).json({ message: "Invalid authorization" });
+        }
+        const token = authorization.split(process.env.BEARERKEY)[1];
+        const decoded = jwt.verify(token, process.env.LOGINTOKEN);
+        if (!decoded) {
+            return res.status(400).json({ message: "Invalid authorization" });
+        }
 
-    }
+        const user = await userModel
+            .findById(decoded.id)
+            .select("role userName changePasswordTime");
 
-    const user=await userModel.findById(decoded.id).select("role userName  changePasswordTime ")
+        if (!user) {
+            return res.status(404).json({ message: "not registerd user" });
+        }
+        
+        if (parseInt(user.changePasswordTime?.getTime() / 1000) > decoded.iat) {
+            return next(new Error("expired token ,plz login again", { cause: 400 }));
+        }
 
-    if(!user){
-        return res.status(404).json({message:"not registerd user"})
-
-    }
-    if(parseInt(user.changePasswordTime?.getTime()/1000)>decoded.iat){ 
-        return next(new Error("expired token ,plz login again",{cause:400}))
-    }
-
-    if(!accessRole.includes(user.role)){
-        return res.status(403).json({message:"not auth user"})
- 
-    }
-    req.user=user
-    next()
-
-}
-}
+        if (!accessRole.includes(user.role)) {
+            return res.status(403).json({ message: "not auth user" });
+        }
+        req.user = user;
+        next();
+    };
+};
--- src/services/multer.js
+++ src/services/multer.js
@@ -1,38 +1,29 @@
-import multer from 'multer';
+import multer from "multer";
 
+export const fileValidation2 = {
+    image: ["image/jpeg", "image/png", "image/webp"],
+    file: ["application/pdf", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
+};
 
-export const fileValidation2={
-    image:['image/jpeg','image/png','image/webp'],
-    file:['application/pdf','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
-    
-}
+function fileupload2(custonValidation = []) {
+    const storage = multer.diskStorage({});
 
-function fileupload2(custonValidation=[]){
-  
-        const storage=multer.diskStorage({});
-    
-  
-  
-   
-    function fileFilter(req,file,cb){
+    function fileFilter(req, file, cb) {
         // if(file.mimetype=='image/jpeg' || file.mimetype=='image/png' || file.mimetype=='image/webp'){
         //     if(['image/jpeg','image/png','image/webp'].includes(file.mimetype)){
         //     cb(null,true);
-        
+
         // }
         //console.log(file)
-        if(custonValidation.includes(file.mimetype)){
-            cb(null,true);
-        }
-        else{
-            cb("invalid format",false) 
+        if (custonValidation.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb("invalid format", false);
         }
     }
-    
-    const upload=multer({fileFilter,storage})
-     
-    return upload;
-   
 
+    const upload = multer({ fileFilter, storage });
+
+    return upload;
 }
 export default fileupload2;
