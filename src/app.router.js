9feece56031276e8f3be9ace3f6876c423ad0ecb
Aseel-Abu-Alrub <aseelabualrub98@gmail.com>
2024-05-23 12:57:15 +0300

    Ecommerce

--- src/app.router.js
+++ src/app.router.js
@@ -17,7 +17,7 @@ const initApp=(app,express)=>{
     connectDB()
     
     app.use(cors({
-        origin:['http://localhost:3000','http://localhost:3001'],
+        origin:['http://localhost:3000','http://localhost:3001','https://ecommerce-g0io.onrender.com'],
         credentials: true,
          methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         // allowedHeaders: ['Content-Type', 'Authorization','origin','X-Requested-With','Accept'],
